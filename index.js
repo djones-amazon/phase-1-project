d962143f792ed06bf84c93e7b572d91c221f6482
Devin Jones <devin.jones21083@gmail.com>
2023-05-14 12:50:39 -0400

    Added form and button to index.html, finished functions to get and filter list of names that will be answer key

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+const filteredNameList = [];
+const imgLocList = [];
+
 
 const getCharList =  async function() {
-    const response = await fetch(`https://api.disneyapi.dev/character?films=The%20Lion%20King`);
+    const response = await fetch(`https://api.disneyapi.dev/character?films=The%20Lion%20King&pageSize=250`);
     const unfilteredData = await response.json();
     
     debugger;
     return (unfilteredData);
 } 
 
-const filterList = async function() {
+const sliceBadNames = function() {
+    const badNames = ['Crocodiles', 'Gopher', `Nala's Father`,
+    'Pride Animals', `Pridelanders`, 'The Hyena Clan', 'Wildebeests'];
+
+    badNames.forEach(badName => {
+        let index = 0;
+        filteredNameList.forEach(filteredName => {
+            if(filteredName == badName) {
+                filteredNameList.splice(index, 1);
+                imgLocList.splice(index, 1);
+            }
+            index++;
+        });
+    });
+    debugger;
+}
+
+const getFilteredList = async function() {
     const targetFilm = 'The Lion King';
     const unfilteredData = await getCharList();
     debugger;
@@ -15,19 +35,22 @@ const filterList = async function() {
     debugger;
     const charFilms = dataArray.map(charObj => charObj.films);
     debugger;
-    const filteredNameList = [];
+    
     let index = 0;
     charFilms.forEach(filmList => {
-        /*if(filmList.includes(targetFilm) == 1) {
+        if(filmList.includes(targetFilm) == 1) {
             filteredNameList.push(dataArray[index].name);
-        }*/
+            imgLocList.push(dataArray[index].imageUrl);            
+        }
         
-        //debugger;
+       // debugger;
         index++;
-        //debugger;
+       // debugger;
     });
-
-    console.log(filteredNameList);
+    debugger;
+    sliceBadNames();
+   // sliceBadNames()
+    //console.log(filteredNameList);
     //const charNamesList = unfilteredList.map(charObj => )
     //const charFilmsList = [];
     
@@ -36,4 +59,4 @@ const filterList = async function() {
 }
 
 
-filterList();
+document.addEventListener('DOMContentLoaded', getFilteredList());
